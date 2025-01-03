3006ed6528d0402ea97dc76faf47b8e093f158a7
ketangopal08 <ketangopal08@gmail.com>
2025-01-03 18:33:26 +0530

    added inspiria

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <BlurReveal
+      :delay="0.2"
+      :duration="0.75"
+      class="p-8"
+    >
+      <h2 class="text-3xl font-bold tracking-tighter xl:text-6xl/none sm:text-5xl">Hey there 👋</h2>
+      <span class="text-pretty text-xl tracking-tighter xl:text-4xl/none sm:text-3xl">
+        How is it going?
+      </span>
+    </BlurReveal>
+  </div>
+</template>
+
+
+<script setup>
+import BlurReveal from '~/components/BlurReveal.vue'
+</script>

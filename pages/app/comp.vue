a42e3f328736d6bfde19659a7d9559b54775dd0b
ketangopal08 <ketangopal08@gmail.com>
2025-01-13 23:52:17 +0530

    new

---
+++ pages/app/comp.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <div class="flex-1 flex flex-col">
+            <header class="bg-gray-100 border-b border-gray-300 p-4">
+                <h1 class="text-lg font-semibold">Welcome Back!</h1>
+            </header>
+          
+        </div>
+    </div>
+</template>
